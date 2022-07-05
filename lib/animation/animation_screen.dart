782e61719257151bfdbe5582028a4a5be434708e
kitashiom <shiori.kitayama@axia-works.com>
2022-07-05 10:20:45 +0900

    AnimationScreenWidget追加

--- lib/animation/animation_screen.dart
+++ lib/animation/animation_screen.dart
@@ -1,18 +1,42 @@
 import 'package:flutter/material.dart';
 
-class AnimationScreen extends StatelessWidget {
+class AnimationScreen extends StatefulWidget {
   const AnimationScreen({Key? key,}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  AnimationScreenState createState() => AnimationScreenState();
+}
 
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Flutter Tutorial'),
-      ),
-      body: Center(
-        child: Text('Tutorial1.1'),
+class AnimationScreenState extends State<AnimationScreen> with SingleTickerProviderStateMixin {
+  late Animation<double> animation;
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = AnimationController(duration: const Duration(seconds: 2), vsync: this);
+    animation = Tween<double>(begin: 0, end: 300).animate(controller);
+    animation.addListener(() {
+      setState(() {});
+    });
+    controller.forward();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Container(
+        margin: const EdgeInsets.symmetric(vertical: 10),
+        height: animation.value,
+        width: animation.value,
+        child: const FlutterLogo(),
       ),
     );
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
   }
 }
