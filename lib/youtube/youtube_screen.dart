27071a4c87d9a58f539dcf8510359089b0472520
kitashiom <shiori.kitayama@axia-works.com>
2022-07-06 16:18:32 +0900

    youtubeスクリーン実装完了しました。

--- lib/youtube/youtube_screen.dart
+++ lib/youtube/youtube_screen.dart
@@ -59,22 +59,64 @@ AppBar _buildAppBar (){
 }
 
 Widget _buildBody (BuildContext context){
+
+  List<Widget> list = [
+    movieItem(),
+    movieItem(),
+    movieItem()
+  ];
+
   return Container(
     width: MediaQuery.of(context).size.width,
-    height: MediaQuery.of(context).size.height,
-    padding: const EdgeInsets.all(10),
     color: const Color(0xff1D191E),
-    child: Column(
-      children: [
-        _categoryButton(context,Icons.local_fire_department,'急上昇'),
-        _categoryButton(context,Icons.music_note,'音楽'),
-        _categoryButton(context,Icons.local_fire_department,'ゲーム'),
-        _categoryButton(context,Icons.local_fire_department,'ニュース'),
-        _categoryButton(context,Icons.local_fire_department,'学び'),
-        _categoryButton(context,Icons.local_fire_department,'ライブ'),
-        _categoryButton(context,Icons.local_fire_department,'スポーツ'),
-        const Text('youtube'),
-      ],
+    child: SingleChildScrollView(
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 5,bottom: 5),
+            child: Wrap(
+              children: [
+                _categoryButton(context,Icons.local_fire_department,'急上昇',
+                    const Color(0xff71122B),const Color(0xffBA1D46)),
+                _categoryButton(context,Icons.music_note,'音楽',
+                    const Color(0xff22675D),const Color(0xff4DAA99)),
+                _categoryButton(context,Icons.sports_esports,'ゲーム',
+                    const Color(0xff7C5353),const Color(0xffC7857E)),
+                _categoryButton(context,Icons.newspaper,'ニュース',
+                    const Color(0xff114676),const Color(0xff0F569D)),
+                _categoryButton(context,Icons.tips_and_updates,'学び',
+                    const Color(0xff125D3E),const Color(0xff128C57)),
+                _categoryButton(context,Icons.live_tv,'ライブ',
+                    const Color(0xffB33B0F),const Color(0xffF78548)),
+                _categoryButton(context,Icons.emoji_events,'スポーツ',
+                    const Color(0xff055464),const Color(0xff0D9CC0)),
+              ],
+            ),
+          ),
+          const Divider(height: 2,thickness: 2,color: Color(0xff39343A),),
+          Container(
+            width: MediaQuery.of(context).size.width,
+            color: const Color(0xff2B272C),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Padding(
+                  padding: EdgeInsets.all(15),
+                  child: Text('急上昇動画',
+                  style: TextStyle(color: Colors.white,fontSize: 20),
+                  ),
+                ),
+                ListView(
+                  shrinkWrap: true,
+                  physics: const NeverScrollableScrollPhysics(),
+                  children: list
+                ),
+              ],
+            ),
+
+          ),
+        ],
+      ),
     ),
   );
 }
@@ -82,27 +124,74 @@ Widget _buildBody (BuildContext context){
 Widget _categoryButton (
     BuildContext context,
     IconData icon,
-    String title){
-  return Container(
-    width: MediaQuery.of(context).size.width / 2,
-    padding: const EdgeInsets.all(10),
-    decoration: BoxDecoration(
-      borderRadius: BorderRadius.circular(10),
-      gradient: const LinearGradient(
-        colors: [
-          Color(0xff71122B), //始まりの色
-          Color(0xffBA1D46), //終わりの色
+    String title,
+    Color colorStart,
+    Color colorEnd){
+  return Padding(
+    padding: const EdgeInsets.all(5),
+    child: Container(
+      width: (MediaQuery.of(context).size.width - 20) / 2,
+      padding: const EdgeInsets.all(8),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        gradient: LinearGradient(
+          colors: [
+            colorStart, //始まりの色
+            colorEnd,//終わりの色
+          ],
+        ),
+      ),
+      child: Row(
+        children: [
+          Icon(icon, color: Colors.white, size: 30),
+          const SizedBox(width: 10),
+          Text(title,
+            style: const TextStyle(color: Colors.white),),
         ],
       ),
     ),
-    child: Row(
-      children: [
-        Icon(icon, color: Colors.white, size: 30),
-        const SizedBox(width: 10),
-        Text(title,
-          style: const TextStyle(color: Colors.white),),
-      ],
-    ),
+  );
+}
+
+Widget movieItem (){
+  return Column(
+    children: [
+      Image.asset('images/youtube_images/movie_image.png'),
+      Padding(
+        padding: const EdgeInsets.fromLTRB(10,10,0,25),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const CircleAvatar(
+              radius: 20,
+              backgroundImage: AssetImage('images/youtube_images/arashi_logo.jpeg'),
+            ),
+            const SizedBox(width: 10),
+            Flexible(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: const [
+                  Text('"This is ARASHI LIVE 2020.12.31" Digest Movie',
+                    maxLines: 2,
+                    style: TextStyle(color: Colors.white),),
+                  Text('ARASHI・127万回視聴・1日前',
+                    style: TextStyle(color: Colors.grey),),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 30,
+              width: 30,
+              child: IconButton(
+                onPressed: (){},
+                padding: const EdgeInsets.all(0),
+                icon: const Icon(Icons.more_vert_outlined,color: Colors.white,size: 18,),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
   );
 }
 
@@ -112,7 +201,7 @@ BottomNavigationBar _buildBottomNavigationBar (){
       BottomNavigationBarItem(icon: Icon(Icons.home_outlined,), label: 'ホーム'),
       BottomNavigationBarItem(icon: Icon(Icons.explore), label: '探索'),
       BottomNavigationBarItem(icon: Icon(Icons.add_circle_outline, size: 40,), label: ''),
-      BottomNavigationBarItem(icon: Icon(Icons.live_tv), label: '登録チャンネル'),
+      BottomNavigationBarItem(icon: Icon(Icons.subscriptions_outlined), label: '登録チャンネル'),
       BottomNavigationBarItem(icon: Icon(Icons.video_library_outlined), label: 'ライブラリ'),
     ],
     type: BottomNavigationBarType.fixed,
