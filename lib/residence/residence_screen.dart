be5ef2f3d23c30640255453bff7200445f95737f
kitashiom <shiori.kitayama@axia-works.com>
2022-07-06 17:07:51 +0900

    ResidenceScreen追加

---
+++ lib/residence/residence_screen.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class ResidenceScreen extends StatelessWidget {
+  const ResidenceScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+    );
+  }
+}
