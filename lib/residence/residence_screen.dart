28b1d9baf7370646a92770f039e86a66f6279ffd
kitashiom <shiori.kitayama@axia-works.com>
2022-07-07 12:24:17 +0900

    BottomNavigationBar実装しました

--- lib/residence/residence_screen.dart
+++ lib/residence/residence_screen.dart
@@ -4,12 +4,15 @@ import 'package:flutter/material.dart';
 class ResidenceScreen extends StatelessWidget {
   const ResidenceScreen({super.key});
 
-  static const greenPointColor = Color(0xff34AFA1);
+  static const pointColorGreen = Color(0xff34AFA1);
+  static const pointColorGrey = Color(0xffC9C9C9);
+  static const badgeBackColor = Color(0xffF54741);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: _buildAppBar(),
+      bottomNavigationBar: _buildBottomNavigationBar(),
     );
   }
 
@@ -26,7 +29,7 @@ class ResidenceScreen extends StatelessWidget {
             label: Text(
               'カウルのおすすめ',
               style: TextStyle(
-                  color: greenPointColor,
+                  color: pointColorGreen,
                   fontWeight: FontWeight.bold,
                   fontSize: 12),
             ),
@@ -34,7 +37,7 @@ class ResidenceScreen extends StatelessWidget {
           const SizedBox(width: 8),
           Badge(
             position: BadgePosition.topEnd(top: 1, end: -3),
-            badgeColor: const Color(0xffF54741),
+            badgeColor: badgeBackColor,
             elevation: 0,
             badgeContent: const Text(
               '1',
@@ -57,11 +60,59 @@ class ResidenceScreen extends StatelessWidget {
           icon: const Icon(
             Icons.add_circle,
             size: 40,
-            color: greenPointColor,
+            color: pointColorGreen,
           ),
           onPressed: () {},
         ),
       ],
+    );
+  }
+
+  BottomNavigationBar _buildBottomNavigationBar() {
+    return BottomNavigationBar(
+      items: <BottomNavigationBarItem>[
+        const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+              size: 35,
+            ),
+            label: 'ホーム'),
+        const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite_border,
+              size: 35,
+            ),
+            label: 'お気に入り'),
+        BottomNavigationBarItem(
+            icon: Badge(
+              position: BadgePosition.topEnd(top: -5, end: -5),
+              badgeColor: badgeBackColor,
+              elevation: 0,
+              badgeContent: const Text(
+                '1',
+                style: TextStyle(color: Colors.white, fontSize: 13),
+              ),
+              child: const Icon(
+                Icons.messenger_outline,
+                size: 35,
+              ),
+            ),
+            label: 'メッセージ'),
+        const BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_outlined,
+              size: 35,
+            ),
+            label: 'マイページ'),
+      ],
+      type: BottomNavigationBarType.fixed,
+      selectedItemColor: pointColorGreen,
+      selectedFontSize: 11,
+      selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
+      unselectedIconTheme: const IconThemeData(color: pointColorGrey),
+      unselectedFontSize: 10,
+      unselectedLabelStyle: const TextStyle(color: Colors.black),
+      backgroundColor: Colors.white,
     );
   }
 }
