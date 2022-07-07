f5a3e64d7d921ae9fea3a2ce94d652d359b5202f
kitashiom <shiori.kitayama@axia-works.com>
2022-07-07 11:53:29 +0900

    Appbar実装完了しました。

--- lib/residence/residence_screen.dart
+++ lib/residence/residence_screen.dart
@@ -1,52 +1,67 @@
+import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
-
-const greenPointColor = Color(0xff34AFA1);
 
 class ResidenceScreen extends StatelessWidget {
   const ResidenceScreen({super.key});
 
+  static const greenPointColor = Color(0xff34AFA1);
+
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: _buildAppBar(),
     );
   }
-}
 
-AppBar _buildAppBar (){
-  return AppBar(
-    backgroundColor: Colors.white,
-    automaticallyImplyLeading: false,
-    centerTitle: false,
-    title: Row(
-      children: const [
-        Chip(
-          backgroundColor: Color(0xffE9EFEE),
-          labelPadding: EdgeInsets.symmetric(horizontal: 10),
-          label: Text('カウルのおすすめ',
-            style: TextStyle(
-                color: greenPointColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 13),),
-        ),
-        SizedBox(width: 8),
-        Chip(
-          backgroundColor: Color(0xffE9EFEE),
-          labelPadding: EdgeInsets.symmetric(horizontal: 10),
-          label: Text('カウルのおすすめ',
-            style: TextStyle(
-                color: greenPointColor,
-                fontWeight: FontWeight.bold,
-                fontSize: 13),),
+  AppBar _buildAppBar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      automaticallyImplyLeading: false,
+      centerTitle: false,
+      title: Row(
+        children: [
+          const Chip(
+            backgroundColor: Color(0xffE9EFEE),
+            labelPadding: EdgeInsets.symmetric(horizontal: 8),
+            label: Text(
+              'カウルのおすすめ',
+              style: TextStyle(
+                  color: greenPointColor,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12),
+            ),
+          ),
+          const SizedBox(width: 8),
+          Badge(
+            position: BadgePosition.topEnd(top: 1, end: -3),
+            badgeColor: const Color(0xffF54741),
+            elevation: 0,
+            badgeContent: const Text(
+              '1',
+              style: TextStyle(color: Colors.white, fontSize: 11),
+            ),
+            child: const Chip(
+              backgroundColor: Color(0xffF3F3F3),
+              labelPadding: EdgeInsets.symmetric(horizontal: 8),
+              label: Text(
+                'リフォーム済みの',
+                style: TextStyle(fontSize: 12),
+              ),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        IconButton(
+          padding: const EdgeInsets.only(right: 16),
+          icon: const Icon(
+            Icons.add_circle,
+            size: 40,
+            color: greenPointColor,
+          ),
+          onPressed: () {},
         ),
       ],
-    ),
-    leadingWidth: 110,
-    actions: [
-      IconButton(
-        icon: const Icon(Icons.add_circle,size: 35,color: greenPointColor,),
-        onPressed: () {},
-      ),
-    ],
-  );
+    );
+  }
 }
