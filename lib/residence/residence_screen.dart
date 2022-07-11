5785198740abaf0a3366b92178c64e4bcfa985d2
kitashiom <shiori.kitayama@axia-works.com>
2022-07-11 16:21:04 +0900

    変数名変更＆カンマなどちょっとした修正

--- lib/residence/residence_screen.dart
+++ lib/residence/residence_screen.dart
@@ -22,11 +22,14 @@ class RoomInfo {
 class ResidenceScreen extends StatelessWidget {
   ResidenceScreen({super.key});
 
-  static const pointColorGreen = Color(0xff34AFA1);
-  static const pointColorGrey = Color(0xffC9C9C9);
-  static const pointColorRed = Color(0xffFB715A);
-  static const pointColorBeige = Color(0xffFAF8F5);
-  static const badgeBackColor = Color(0xffF54741);
+  static const green = Color(0xff34AFA1);
+  static const grey = Color(0xffC9C9C9);
+  static const lightGrey = Color(0xffE9EFEE);
+  static const silverWhite = Color(0xffF3F3F3);
+  static const red = Color(0xffF54741);
+  static const lightRed = Color(0xffFB715A);
+  static const beige = Color(0xffFAF8F5);
+  static const lightBlack = Color(0xff666666);
   static const SizedBox spaceW4 = SizedBox(
     width: 4,
   );
@@ -89,12 +92,12 @@ class ResidenceScreen extends StatelessWidget {
       title: Row(
         children: [
           const Chip(
-            backgroundColor: Color(0xffE9EFEE),
+            backgroundColor: lightGrey,
             labelPadding: EdgeInsets.symmetric(horizontal: 8),
             label: Text(
               'カウルのおすすめ',
               style: TextStyle(
-                color: pointColorGreen,
+                color: green,
                 fontWeight: FontWeight.bold,
                 fontSize: 12,
               ),
@@ -103,14 +106,14 @@ class ResidenceScreen extends StatelessWidget {
           spaceW8,
           Badge(
             position: BadgePosition.topEnd(top: 1, end: -3),
-            badgeColor: badgeBackColor,
+            badgeColor: red,
             elevation: 0,
             badgeContent: const Text(
               '1',
               style: TextStyle(color: Colors.white, fontSize: 11),
             ),
             child: const Chip(
-              backgroundColor: Color(0xffF3F3F3),
+              backgroundColor: silverWhite,
               labelPadding: EdgeInsets.symmetric(horizontal: 8),
               label: Text(
                 'リフォーム済みの',
@@ -126,7 +129,7 @@ class ResidenceScreen extends StatelessWidget {
           icon: const Icon(
             Icons.add_circle,
             size: 40,
-            color: pointColorGreen,
+            color: green,
           ),
           onPressed: () {},
         ),
@@ -137,7 +140,7 @@ class ResidenceScreen extends StatelessWidget {
   Widget _buildBody(BuildContext context) {
     return Container(
       padding: const EdgeInsets.only(top: 8),
-      color: pointColorBeige,
+      color: beige,
       child: SingleChildScrollView(
         child: Column(
           children: [
@@ -164,12 +167,14 @@ class ResidenceScreen extends StatelessWidget {
         Icon(
           icon,
           size: 16,
-          color: const Color(0xff666666),
+          color: lightBlack,
         ),
         spaceW4,
         Text(
           text,
-          style: const TextStyle(fontSize: 11),
+          style: const TextStyle(
+            fontSize: 11,
+          ),
         ),
       ],
     );
@@ -183,7 +188,7 @@ class ResidenceScreen extends StatelessWidget {
         borderRadius: BorderRadius.circular(10),
       ),
       child: Padding(
-        padding: const EdgeInsets.all(8),
+        padding: const EdgeInsets.all(8.0),
         child: Column(
           children: [
             Row(
@@ -191,20 +196,27 @@ class ResidenceScreen extends StatelessWidget {
                 spaceW8,
                 const Text(
                   'カウルのおすすめ',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 13),
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 13,
+                  ),
                 ),
                 spaceW8,
                 const Text(
                   '新着３件',
                   style: TextStyle(
-                      color: pointColorRed,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 12),
+                    color: lightRed,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 12,
+                  ),
                 ),
                 const Spacer(),
                 const Text(
                   '編集',
-                  style: TextStyle(color: pointColorGreen, fontSize: 12),
+                  style: TextStyle(
+                    color: green,
+                    fontSize: 12,
+                  ),
                 ),
                 SizedBox(
                   height: 30,
@@ -214,7 +226,7 @@ class ResidenceScreen extends StatelessWidget {
                       padding: EdgeInsets.zero,
                       icon: const Icon(
                         Icons.edit,
-                        color: pointColorGreen,
+                        color: green,
                         size: 25,
                       )),
                 ),
@@ -222,19 +234,28 @@ class ResidenceScreen extends StatelessWidget {
             ),
             Card(
               elevation: 0,
-              color: pointColorBeige,
+              color: beige,
               shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.circular(10),
               ),
               child: Padding(
-                padding: const EdgeInsets.all(8),
+                padding: const EdgeInsets.all(8.0),
                 child: Column(
                   children: [
-                    _detailRow(Icons.train, '東京駅・品川駅・川崎駅・横浜駅・目黒駅・恵比寿駅・渋谷駅・'),
+                    _detailRow(
+                      Icons.train,
+                      '東京駅・品川駅・川崎駅・横浜駅・目黒駅・恵比寿駅・渋谷駅・',
+                    ),
                     spaceH4,
-                    _detailRow(Icons.paid, '下限なし 〜 2,000万円'),
+                    _detailRow(
+                      Icons.paid,
+                      '下限なし 〜 2,000万円',
+                    ),
                     spaceH4,
-                    _detailRow(Icons.info_outline, '1R 〜 4LDK / 10㎡以上 / 徒歩20分'),
+                    _detailRow(
+                      Icons.info_outline,
+                      '1R 〜 4LDK / 10㎡以上 / 徒歩20分',
+                    ),
                   ],
                 ),
               ),
@@ -264,14 +285,17 @@ class ResidenceScreen extends StatelessWidget {
                 Text(
                   roomInfo.title,
                   style: const TextStyle(
-                      fontSize: 18, fontWeight: FontWeight.bold),
+                    fontSize: 18,
+                    fontWeight: FontWeight.bold,
+                  ),
                 ),
                 Text(
                   roomInfo.price,
                   style: const TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                      color: pointColorRed),
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: lightRed,
+                  ),
                 ),
                 spaceH4,
                 _detailRow(Icons.train, roomInfo.access),
@@ -291,14 +315,14 @@ class ResidenceScreen extends StatelessWidget {
                         8,
                       ),
                       decoration: BoxDecoration(
-                        border: Border.all(color: pointColorGrey),
+                        border: Border.all(color: grey),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Row(
                         children: const [
                           Icon(
                             Icons.delete,
-                            color: pointColorGrey,
+                            color: grey,
                           ),
                           spaceW16,
                           Text('興味なし'),
@@ -314,14 +338,14 @@ class ResidenceScreen extends StatelessWidget {
                         8,
                       ),
                       decoration: BoxDecoration(
-                        border: Border.all(color: pointColorGrey),
+                        border: Border.all(color: grey),
                         borderRadius: BorderRadius.circular(10),
                       ),
                       child: Row(
                         children: const [
                           Icon(
                             Icons.favorite_border,
-                            color: pointColorGrey,
+                            color: grey,
                           ),
                           spaceW16,
                           Text('お気に入り'),
@@ -343,7 +367,7 @@ class ResidenceScreen extends StatelessWidget {
       height: 64,
       width: 64,
       child: FloatingActionButton.extended(
-        backgroundColor: pointColorGreen,
+        backgroundColor: green,
         elevation: 3,
         onPressed: () {},
         label: Column(
@@ -354,7 +378,10 @@ class ResidenceScreen extends StatelessWidget {
             ),
             Text(
               '物件',
-              style: TextStyle(fontSize: 11, fontWeight: FontWeight.bold),
+              style: TextStyle(
+                fontSize: 11,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ],
         ),
@@ -380,11 +407,14 @@ class ResidenceScreen extends StatelessWidget {
         BottomNavigationBarItem(
             icon: Badge(
               position: BadgePosition.topEnd(top: -5, end: -5),
-              badgeColor: badgeBackColor,
+              badgeColor: red,
               elevation: 0,
               badgeContent: const Text(
                 '1',
-                style: TextStyle(color: Colors.white, fontSize: 13),
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 13,
+                ),
               ),
               child: const Icon(
                 Icons.messenger_outline,
@@ -400,10 +430,10 @@ class ResidenceScreen extends StatelessWidget {
             label: 'マイページ'),
       ],
       type: BottomNavigationBarType.fixed,
-      selectedItemColor: pointColorGreen,
+      selectedItemColor: green,
       selectedFontSize: 11,
       selectedLabelStyle: const TextStyle(fontWeight: FontWeight.bold),
-      unselectedIconTheme: const IconThemeData(color: pointColorGrey),
+      unselectedIconTheme: const IconThemeData(color: grey),
       unselectedFontSize: 10,
       unselectedLabelStyle: const TextStyle(color: Colors.black),
       backgroundColor: Colors.white,
