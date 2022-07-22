6db7877c894008e376c4e85252700c0494ae37ac
kitashiom <shiori.kitayama@axia-works.com>
2022-07-22 10:22:25 +0900

    API取得&反映

---
+++ lib/mercari/mercari_client_state_notifier.dart
@@ -0,0 +1,33 @@
+import 'package:axiaworks_flutter_tutorial/mercari/repository/mercari_repository.dart';
+import 'package:axiaworks_flutter_tutorial/mercari/state/mercari_client_state.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final mercariClientStateNotifier = StateNotifierProvider.autoDispose<
+    MercariClientStateNotifier,
+    MercariClientState>((ref) => MercariClientStateNotifier(ref.read));
+
+class MercariClientStateNotifier extends StateNotifier<MercariClientState> {
+  MercariClientStateNotifier(this._read) : super(const MercariClientState()) {
+    fetchMercariItems();
+  }
+
+  final Reader _read;
+
+  Future<void> fetchMercariItems() async {
+    state = state.copyWith(isLoading: true);
+
+    final mercariItems =
+        await _read(mercariRepositoryProvider).fetchMercariItems();
+
+    if (mercariItems.isNotEmpty) {
+      state = state.copyWith(
+          isLoading: false, isReadyData: true, mercariItems: mercariItems);
+    } else {
+      state = state.copyWith(
+        isLoading: false,
+        isReadyData: false,
+        mercariItems: mercariItems,
+      );
+    }
+  }
+}
