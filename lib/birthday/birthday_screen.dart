07ceb70f6fda2a080cf61f0e0063db1f8a5da386
kitashiom <shiori.kitayama@axia-works.com>
2022-08-03 20:42:15 +0900

    Widget整理

--- lib/birthday/birthday_screen.dart
+++ lib/birthday/birthday_screen.dart
@@ -14,7 +14,7 @@ class Birthday {
 }
 
 class BirthdayScreen extends ConsumerWidget {
-  const BirthdayScreen({super.key});
+  BirthdayScreen({super.key});
 
   static const pink = Color(0xffFBA1A1);
   static const spaceW8 = SizedBox(
@@ -24,31 +24,31 @@ class BirthdayScreen extends ConsumerWidget {
     height: 16,
   );
 
+  final List<Birthday> list = [
+    Birthday(
+      name: 'misumi',
+      birthday: '12/6',
+      gift: 'リップ＆靴',
+    ),
+    Birthday(
+      name: 'mirei',
+      birthday: '2/24',
+      gift: 'リップ＆靴',
+    ),
+    Birthday(
+      name: 'natsuki',
+      birthday: '8/8',
+      gift: 'リップ＆靴',
+    ),
+    Birthday(
+      name: 'natsuki',
+      birthday: '8/8',
+      gift: 'リップ＆靴',
+    ),
+  ];
+
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final list = [
-      Birthday(
-        name: 'misumi',
-        birthday: '12/6',
-        gift: 'リップ＆靴',
-      ),
-      Birthday(
-        name: 'mirei',
-        birthday: '2/24',
-        gift: 'リップ＆靴',
-      ),
-      Birthday(
-        name: 'natsuki',
-        birthday: '8/8',
-        gift: 'リップ＆靴',
-      ),
-      Birthday(
-        name: 'natsuki',
-        birthday: '8/8',
-        gift: 'リップ＆靴',
-      ),
-    ];
-
     return Scaffold(
       appBar: AppBar(
         title: const Text("My friend's Birthday"),
@@ -81,108 +81,7 @@ class BirthdayScreen extends ConsumerWidget {
                 physics: const NeverScrollableScrollPhysics(),
                 itemBuilder: (context, index) {
                   final item = list[index];
-                  return SizedBox(
-                    child: Stack(
-                      clipBehavior: Clip.none,
-                      children: [
-                        Card(
-                          shape: RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(24),
-                          ),
-                          child: Padding(
-                            padding: const EdgeInsets.all(16),
-                            child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(
-                                      item.birthday,
-                                      style: const TextStyle(
-                                        fontSize: 32,
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                    SizedBox(
-                                      height: 20,
-                                      width: 25,
-                                      child: IconButton(
-                                        onPressed: () {},
-                                        alignment: Alignment.topRight,
-                                        icon: const Icon(Icons.more_horiz),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                spaceH16,
-                                Align(
-                                  child: Text(
-                                    item.name,
-                                    style: const TextStyle(
-                                      fontSize: 18,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                ),
-                                Row(
-                                  children: const [
-                                    Icon(
-                                      Icons.navigate_next,
-                                      color: pink,
-                                    ),
-                                    spaceW8,
-                                    Text(
-                                      '21歳',
-                                      style: TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                                Row(
-                                  children: [
-                                    const Icon(
-                                      Icons.redeem,
-                                      color: pink,
-                                    ),
-                                    spaceW8,
-                                    Text(
-                                      item.gift,
-                                      style: const TextStyle(
-                                        fontWeight: FontWeight.bold,
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        ),
-                        Positioned(
-                          top: -30,
-                          left: (MediaQuery.of(context).size.width - 116) / 2,
-                          child: Container(
-                            width: 100,
-                            height: 100,
-                            decoration: const BoxDecoration(
-                              color: Colors.white,
-                              shape: BoxShape.circle,
-                            ),
-                            child: const Text(
-                              '👩🏻',
-                              style: TextStyle(
-                                fontSize: 40,
-                              ),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      ],
-                    ),
-                  );
+                  return _buildListView(context, item);
                 },
                 separatorBuilder: (BuildContext context, int index) {
                   return const SizedBox(
@@ -193,6 +92,110 @@ class BirthdayScreen extends ConsumerWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+
+  Widget _buildListView(BuildContext context, Birthday item) {
+    return SizedBox(
+      child: Stack(
+        clipBehavior: Clip.none,
+        children: [
+          Card(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(24),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        item.birthday,
+                        style: const TextStyle(
+                          fontSize: 32,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      SizedBox(
+                        height: 20,
+                        width: 25,
+                        child: IconButton(
+                          onPressed: () {},
+                          alignment: Alignment.topRight,
+                          icon: const Icon(Icons.more_horiz),
+                        ),
+                      ),
+                    ],
+                  ),
+                  spaceH16,
+                  Align(
+                    child: Text(
+                      item.name,
+                      style: const TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Row(
+                    children: const [
+                      Icon(
+                        Icons.navigate_next,
+                        color: pink,
+                      ),
+                      spaceW8,
+                      Text(
+                        '21歳',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    children: [
+                      const Icon(
+                        Icons.redeem,
+                        color: pink,
+                      ),
+                      spaceW8,
+                      Text(
+                        item.gift,
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Positioned(
+            top: -30,
+            left: (MediaQuery.of(context).size.width - 116) / 2,
+            child: Container(
+              width: 100,
+              height: 100,
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                shape: BoxShape.circle,
+              ),
+              child: const Text(
+                '👩🏻',
+                style: TextStyle(
+                  fontSize: 48,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
